21766fb167d4baf2fd70940a06f6f3009db494b2
Benoit Mignault <b.mignault@gmail.com>
2018-02-02 00:42:58 -0500

    Ajout de la version venant de mon portable

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class HelloWorld {
 
     public static void main(String[] args) { 
-        
+        //voici une version du portable via netbeam
         Biblio.lireFichier("src/PartageLesAmis/biblio.txt"); 
           try { 
                 BufferedReader br = new BufferedReader(new FileReader("src/PartageLesAmis/biblio.txt"));
