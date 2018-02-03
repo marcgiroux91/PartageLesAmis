6f09d12cb34980612903213b432372e0108b90d7
benoitmignault <b.mignault@gmail.com>
2018-02-03 00:16:11 -0500

    MAJ

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,18 +6,19 @@ public class HelloWorld {
 
     public static void main(String[] args) { 
         //voici une version du portable via netbeam
+        // voici les informations que j'ajouter sur l'autre branche 
         Biblio.lireFichier("src/PartageLesAmis/biblio.txt"); 
-          try { 
-                BufferedReader br = new BufferedReader(new FileReader("src/PartageLesAmis/biblio.txt"));
-                String line = br.readLine();                
-                while (line != null) {
-                    System.out.println(line);                    
-                    line = br.readLine();
-                }                
-                br.close();       
-            } catch (Exception ex ) {
-                System.out.println("Problème d'ouverture du fichier" + "biblio.txt" );
-            } 
-          
+        try { 
+            BufferedReader br = new BufferedReader(new FileReader("src/PartageLesAmis/biblio.txt"));
+            String line = br.readLine();                
+            while (line != null) {
+                System.out.println(line);                    
+                line = br.readLine();
+            }                
+            br.close();       
+        } catch (Exception ex ) {
+            System.out.println("Problème d'ouverture du fichier" + "biblio.txt" );
+        } 
+
     }
 }
