2bcdf8bf8eba40f7665c9d16e1e09cc60a4c8c9a
Benoit Mignault <b.mignault@gmail.com>
2018-02-08 20:47:54 -0500

    Ajout une ligne de teste

--- Biblio.java
+++ Biblio.java
@@ -4,19 +4,20 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class Biblio {
-    public static void lireFichier(String nom){
-        try { 
-                BufferedReader br = new BufferedReader(new FileReader(nom));
-                
-                String line = br.readLine();                
-                while (line != null) {
-                    System.out.println(line);                    
-                    line = br.readLine();
-                }                
-                br.close();       
-            } catch (Exception ex ) {
-                System.out.println("Problème d'ouverture du fichier" + "biblio.txt" );
-            } 
+    //bonjour marc voici un test pour un merge via github
+    public static void lireFichier(String nom) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(nom));
+            
+            String line = br.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+            br.close();
+        } catch (Exception ex) {
+            System.out.println("Problème d'ouverture du fichier" + "biblio.txt");
+        }
     }
     
 }
